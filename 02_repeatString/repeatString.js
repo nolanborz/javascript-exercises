1b8a003e7a239490086cc84b03b4f6acb026477a
nolanborzoni <nolanrborzoni@gmail.com>
2024-05-07 19:20:53 +0900

    Finish first 3 exercises of javascript-exercises
    Finished helloWorld, repeatString, and reverseString
    of the project. Went easily.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, numberOfReps) {
+    let newString = '';
+    if (numberOfReps < 0) { 
+        return 'ERROR'
+    }
+    else {
+    for ( i=0; i < numberOfReps; i++) {
+        newString += phrase;
+    }
+    return newString;
+    }    
 };
 
 // Do not edit below this line
