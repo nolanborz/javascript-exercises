97ef33d7a934cd4ed822793859c66c7244df26ea
nolanborzoni <nolanrborzoni@gmail.com>
2024-05-08 20:02:02 +0900

    Finish exercise 5 about writing sum of range func
    Wrote a function that finds the sum of numbers
    within a range, as well as a few safeguards that
    covers different possibilities of entered parameters.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
-
-};
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0) {
+        return 'ERROR';
+    }
+    if (typeof int1 != 'number' || typeof int2 != 'number') {
+        return 'ERROR';
+    }
+    let newRange = [];
+    let newSum = 0;
+    for (let currentPoint = int1 < int2 ? int1: int2; currentPoint <= findMax(int1, int2); currentPoint++) {
+        newSum += currentPoint
+    }
+    return newSum;
+}
+function findMax (x, y) {
+    if (x > y) {
+        return x;
+    }
+    else { 
+        return y; 
+    }
+}
+//const sumAll = function(int1, int2) {
+//    let newRange = [];
+//    let newSum = 0;
+//    for (let currentPoint = int1; currentPoint <= int2; currentPoint++) {
+//        newSum += currentPoint
+//    }
+//   return newSum;
+//}
 
 // Do not edit below this line
 module.exports = sumAll;
